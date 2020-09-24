7fa08f1280ef27964ca5284eeda70756b151fc0a
post45 <bashkuresh92@yandex.ru>
2020-09-24 16:37:08 -0400

    header logout commit

--- react-test-blog/src/header/Header.js
+++ react-test-blog/src/header/Header.js
@@ -1,13 +1,84 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Limk, Link } from "react-router-dom";
+import { logout } from "../actions/UserActions";
 
 class Header extends Component {
+  logout() {
+    this.props.logout();
+    window.location.href = "/";
+  }
   render() {
+    const { validToken, user } = this.props.security;
+
+    const userIsAuthenticated = (
+      <div className="collapse navbar-collapse" id="mobile-nav">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/dashboard">
+              Dashboard
+            </Link>
+          </li>
+        </ul>
+
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/dashboard">
+              <i className="fas fa-user-circle mr-1" />
+              {user.firstName}
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              className="nav-link"
+              to="/logout"
+              onClick={this.logout.bind(this)}
+            >
+              Logout
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+
+    {
+      //
+    }
+    const userIsNotAuthenticated = (
+      <div className="collapse navbar-collapse" id="mobile-nav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/register">
+              Sign Up
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/login">
+              Login
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+
+    let headerLinks;
+
+    if (validToken && user) {
+      headerLinks = userIsAuthenticated;
+    } else {
+      headerLinks = userIsNotAuthenticated;
+    }
+    {
+      //
+    }
+
     return (
       <nav className="navbar navbar-expand-sm navbar-dark bg-success mb-4">
         <div className="container">
-          <a className="navbar-brand" href="Dashboard">
+          <Link className="navbar-brand" to="/">
             Spring Blog
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -20,28 +91,28 @@ class Header extends Component {
           <div className="collapse navbar-collapse" id="mobile-nav">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-                <a className="nav-link" href="/AddPost">
+                <Link className="nav-link" to="/AddPost">
                   Add Post
-                </a>
+                </Link>
               </li>
             </ul>
 
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <a className="nav-link " href="register">
+                <Link className="nav-link " to="register">
                   Sign Up
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="login">
+                <Link className="nav-link" to="login">
                   Login
-                </a>
+                </Link>
               </li>
 
               <li className="nav-item">
-                <a className="nav-link" href="login">
+                <Link className="nav-link" to="login">
                   Logout
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
@@ -50,4 +121,12 @@ class Header extends Component {
     );
   }
 }
-export default Header;
+Header.propTypes = {
+  logout: PropTypes.func.isRequired,
+  security: PropTypes.object.isRequired,
+};
+const mapStateToProps = (state) => ({
+  security: state.security,
+});
+
+export default connect(mapStateToProps, { logout })(Header);
